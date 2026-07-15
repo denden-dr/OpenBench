4250c4085406a17b4b20a7593394a391cc4e8e4d
denden-dr <deni.rianto0208@gmail.com>
2026-07-15 11:12:02 +0700

    refactor(config): replace manual config validation with declarative struct tags

--- apps/backend/config/authconfig.go
+++ apps/backend/config/authconfig.go
@@ -3,8 +3,8 @@ package config
 import "time"
 
 type AuthConfig struct {
-	AccessSecret  string        `mapstructure:"access_secret"`
-	RefreshSecret string        `mapstructure:"refresh_secret"`
+	AccessSecret  string        `mapstructure:"access_secret" validate:"required,min=32,excludes=your_super_secret"`
+	RefreshSecret string        `mapstructure:"refresh_secret" validate:"required,min=32,excludes=your_super_secret"`
 	AccessExpiry  time.Duration `mapstructure:"access_expiry"`
 	RefreshExpiry time.Duration `mapstructure:"refresh_expiry"`
 }
